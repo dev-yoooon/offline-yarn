a29c7223d2396bc54f28a4822a4ddb8a3f512ca1
dev.yoooon <99169513+dev-yoooon@users.noreply.github.com>
2023-04-24 08:01:46 +0000

    change folder dir

--- gulpfile.js
+++ gulpfile.js
@@ -11,25 +11,36 @@ const setProduct = async () => {
 }
 
 
+const base = {
+  src: './user',
+  dist: './dist',
+  guide: './user/guide',
+}
 const dir = {
   src: {
-    base: './src',
-    html: './src/**/!(_)*.{html,ejs}',
-    scss: './src/assets/scss/**/*.scss',
-    js: './src/assets/js/**/*.js',
-    images: './src/assets/images/**/*',
-    fonts: './src/assets/fonts/**/*',
+    html: `${base.src}/**/!(_)*.{html,ejs}`,
+    scss: `${base.src}/_sass/**/*.scss`,
+    css: `${base.src}/**/*.css`,
+    js: `${base.src}/js/**/*.js`,
+    images: `${base.src}/images/**/*`,
+    fonts: `${base.src}/fonts/**/*`,
+  },
+  guide: {
+    html: `${base.guide}/**/*.html`,
+    copy: `${base.guide}/**/*.{css,js}`
   },
   dist: {
-    base: './dist',
-    css: './dist/assets/css',
-    js: './dist/assets/js/',
-    images: './dist/assets/images/',
+    scss: `${base.dist}/_sass`,
+    css: `${base.dist}`,
+    js: `${base.dist}/js`,
+    images: `${base.dist}/images`,
+    fonts: `${base.dist}/fonts`,
+    guide: `${base.dist}/guide`
   }
 }
 
 const clean = () => {
-	return src(['./dist'])
+	return src([base.dist])
 		.pipe($.clean())
 }
 
@@ -37,19 +48,19 @@ const server = async () => {
   $.connect.server({
     host: ip.address(),
     port: '1119',
-    root: dir.dist.base,
+    root: base.dist,
     livereload: true,
   })
 }
 
 const html = async () => { 
-  return src([dir.src.html])
+  return src([dir.src.html, dir.guide.html])
 		.pipe($.plumber())
     .pipe($.fileInclude())
     .pipe($.htmlTagInclude())
     .pipe($.ejs())
     .pipe($.prettier())
-    .pipe(dest(dir.dist.base))
+    .pipe(dest(base.dist))
     .pipe($.connect.reload())
 }
 
@@ -67,9 +78,29 @@ const scss = async () => {
 
 const image = async () => {
   return src([dir.src.images], {since: lastRun(image)})
-    .pipe($.newer(dir.dist.base))
+    .pipe($.newer(base.dist))
 		.pipe($.cached(dir.src.base))
-    .pipe(dest(dir.dist.base))
+    .pipe(dest(dir.dist.images))
+}
+
+const js = async () => {
+  return src([dir.src.js])
+    .pipe(dest(dir.dist.js))
+}
+
+const font = async () => {
+  return src([dir.src.fonts])
+    .pipe(dest(dir.dist.fonts))
+}
+
+const copyScss = async () => {
+  return src([dir.src.scss]).pipe(dest(dir.dist.scss))
+}
+const copyCss = async () => {
+  return src([dir.src.css]).pipe(dest(dir.dist.css));
+}
+const copyGuide = async () => {
+  return src([dir.guide.copy]).pipe(dest(dir.dist.guide));
 }
 
 const watcher = async () => {
@@ -77,7 +108,10 @@ const watcher = async () => {
   watch([ dir.src.scss ], scss)
 }
 
-const dev = series(html, scss, image, parallel(watcher, server));
+const copy = parallel(copyCss, copyScss, copyGuide);
+const webServer = parallel(watcher, server); 
+const dev = series(html, scss, image, font, js, copy);
 exports.clean = series(clean);
+exports.copy = series(copy);
 exports.build = series(setProduct, dev);
-exports.default = dev;
+exports.default = series(dev, webServer);
