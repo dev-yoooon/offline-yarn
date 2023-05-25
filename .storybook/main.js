be218254eabcac6daa8fb77468b00bf535cb533e
yoooon <dev.yoooon@gmail.com>
2023-05-25 22:38:01 +0900

    add prism

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,24 +1,13 @@
-/** @type { import('@storybook/web-components-webpack5').StorybookConfig } */
-const path = require('path');
-
+/** @type { import('@storybook/web-components-vite').StorybookConfig } */
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   framework: {
-    name: "@storybook/web-components-webpack5",
+    name: "@storybook/web-components-vite",
     options: {},
   },
   docs: {
     autodocs: "tag",
   },
-  // webpackFinal: async (config, { configType }) => {
-  // config.module.rules.push({
-  //   test: /\.ejs$/,
-  //   use: ['ejs-compiled-loader'],
-  //   include: path.resolve(__dirname, '../html/inc')
-  // })
-  // console.log(__dirname);
-  // return config
-// }
 };
 export default config;
